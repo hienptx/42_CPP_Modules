894fdad9b696c5b5871257c4f4edf7970852ec9e
Hien Pham <hipham@1-B-12.42heilbronn.de>
2024-11-05 15:07:56 +0100

    ex01: in process

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,31 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hipham <hipham@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 12:36:17 by hipham            #+#    #+#             */
+/*   Updated: 2024/11/05 15:06:11 by hipham           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
+#include <stdio.h>
+#include <string>
 
-// namespace example
-
-namespace func1
+namespace darkest_secret
 {
-    void print(int a)
+    void	get_info(void)
     {
-        std::cout << "a = " << a << std::endl;
+        std::string darkest_secret;
+        std::cout << "darkest_secret: ";
+        std::cin >> darkest_secret;
     }
 }
 
-namespace func2
+namespace phone_number
 {
-    void print(int *a)
+    void	get_info(void)
     {
-        *a = *a + 1;
-        std::cout << "increment a = " << *a << std::endl;
-    }
+        int	valid;
 
+        std::string phone_number;
+        valid = 0;
+        while (!valid)
+        {
+            std::cout << "phone_number: ";
+            std::cin >> phone_number;
+            valid = 1;
+            for (int i = 0; phone_number[i] != '\0'; i++)
+            {
+                if (!isdigit(phone_number[i]))
+                {
+                    phone_number.clear();
+                    valid = 0;
+                }
+            }
+        }
+    }
 }
 
-int main()
+namespace nick_name
 {
-    int a = 2;
+void	get_info(void)
+{
+	std::string nick_name;
+	std::cout << "nick_name: ";
+	std::cin >> nick_name;
+}
+}
 
-    func1::print(a);
-    func2::print(&a);
+namespace last_name
+{
+    void	get_info(void)
+    {
+        std::string last_name;
+        std::cout << "last_name: ";
+        std::cin >> last_name;
+    }
+}
 
-    return(0);
+namespace first_name
+{
+    void	get_info(void)
+    {
+        std::string first_name;
+        std::cout << "first_name: ";
+        std::cin >> first_name;
+    }
+}
+
+int	main(void)
+{
+	while (1)
+	{
+		std::string input;
+		std::cout << "Enter your valid actions: ADD, SEARCH, EXIT" << std::endl;
+		std::cin >> input;
+		if (input.compare("ADD") == 0)
+		{
+			first_name::get_info();
+			last_name::get_info();
+			nick_name::get_info();
+			phone_number::get_info();
+			darkest_secret::get_info();
+			continue ;
+		}
+		else if (input.compare("SEARCH") == 0)
+		{
+			;
+		}
+		else if (input.compare("EXIT") == 0)
+		{
+			break ;
+		}
+		else
+			continue ;
+	}
+	return (0);
 }
